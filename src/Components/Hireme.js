3ba619dd375b147954b24597e30944df26c59df8
Chinweike99 <innocentakwolu1@gmail.com>
2025-01-10 19:42:31 +0100

    padding

--- src/Components/Hireme.js
+++ src/Components/Hireme.js
@@ -6,10 +6,10 @@ import React from 'react'
 const Hireme = () => {
   return (
     <div className=' bottom-[10rem] flex items-center justify-center overflow-hidden'>
-        <div className='w-40 h-auto flex items-center justify-center relative'>
+        <div className='w-48 h-auto flex items-center justify-center relative'>
             <Image src={assets.webDeveloper} className='fill-black animate-spin-slow'/>
 
-            <Link href={"/"} className='flex items-center justify-center absolute bg-black text-white font-semibold p-1 rounded'
+            <Link href={"/"} className='flex items-center justify-center absolute bg-black text-white font-semibold p-1 rounded-full h-[5rem] w-[5rem]'
             
             >Hire Me</Link>
         </div>
