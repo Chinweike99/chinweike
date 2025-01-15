ba2502c931dd5723e7e048f7c488a9e6fa6550c7
Chinweike99 <innocentakwolu1@gmail.com>
2025-01-15 11:05:40 +0100

    projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,13 +6,20 @@ import Link from 'next/link'
 import React from 'react'
 import GitHubIcon from "@mui/icons-material/GitHub";
 import { assets } from '@/assets/assets'
-
+import { motion } from 'framer-motion'
 
 
 
 const MyProjects = ({type, title, summary, img, link, github}) => {
     return(
-        <article className='w-full flex items-center justify-between rounded-[1.2rem] overflow-hidden border border-solid border-black shadow-2xl mb-5'>
+        <motion.article className='relative w-full rounded-[1.2rem] rounded-br-2xl mt-[4rem]  shadow-2xl'
+        initial={{y:50}}
+        whileInView={{y:0}}
+        transition={{duration:1.5, type: "spring"}}
+        >
+            
+            <div className="absolute top-0  -z-10 w-[101%] h-[104%] rounded-[2rem] bg-black" />
+            <div className='relative w-full flex bg-white items-center justify-between rounded-[1.2rem] overflow-hidden border border-solid border-black shadow-2xl'>
             <Link href={link} target='_blank'
             className='w-1/2 cursor-pointer overflow-hidden '
             >
@@ -35,10 +42,47 @@ const MyProjects = ({type, title, summary, img, link, github}) => {
                 </div>
 
             </div>
-        </article>
+            </div>
+        </motion.article>
     )
 }
 
+const Project = ({title, type, img, link, summary, github}) =>{
+    return (
+<motion.article className='relative w-full h-[130%]  rounded-[1.2rem] rounded-br-2xl   shadow-2xl'
+initial={{y:50}}
+whileInView={{y:0}}
+transition={{duration:1.5, type: "spring"}}
+>        
+        <div className="absolute top-0  -z-10 w-[101%] h-[104%] rounded-[2rem] bg-black" />
+        <div className='relative w-full h-[100%] flex bg-white items-center justify-between rounded-[1.2rem] overflow-hidden border border-solid border-black shadow-2xl mb-5'>
+        <Link href={link} target='_blank'
+        className='w-full cursor-pointer overflow-hidden '
+        >
+            <Image src={img} alt={title} className='w-full h-auto overflow-hidden' />
+        </Link>
+        {/* <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-black" /> */}
+        <div className='w-full flex flex-col items-start justify-between pl-6'>
+            <span className='text-primary font-medium text-xl'>{type}</span>
+            <Link href={link} target='_blank'>
+                <h2 className='my-2 w-full text-left text-2xl font-bold'>{title}</h2>
+            </Link>
+            <p className='my-2 font-medium text-black'>{summary}</p>
+            <div className='mb-5 flex items-center'>
+                <Link href={github} target='_blank' >
+                    <GitHubIcon />
+                </Link>
+                <Link href={github} target='_blank'className='ml-2 underline font-semibold'>
+                    visit
+                </Link>
+            </div>
+
+        </div>
+        </div>
+    </motion.article>
+
+    )
+}
 
 
 
@@ -57,9 +101,9 @@ const projects = () => {
             <Layout className='pt-10'>
                 <AnimatedText text={"Imagination Trumps Knowledge"} className='mb-5' />
 
-                <div className='grid grid-cols-12 gap-24'>
+                <div className='grid grid-cols-12 gap-24 gap-y-[2rem]'>
 
-                    <div className='col-span-12'>
+                    <div className='col-span-12 gap-y-[2rem]'>
                         <MyProjects 
                         title={"E-commerce site"}
                         summary={"Site for buying and selling"}
@@ -70,7 +114,42 @@ const projects = () => {
                         
                         />
 
-                        <MyProjects 
+                        {/* <MyProjects 
+                        title={"Food delivery website"}
+                        summary={"Site for buying and selling"}
+                        link={'https://github.com/Chinweike99/taSty'}
+                        type={"My project"}
+                        github={"https://github.com/Chinweike99/taSty"}
+                        img={assets.tasty}
+                        
+                        /> */}
+                    </div>
+                    {/* Main Projects */}
+                    <div className='col-span-6'>
+                    <Project 
+                        title={"Food delivery website"}
+                        summary={"Site for buying and selling"}
+                        link={'https://github.com/Chinweike99/taSty'}
+                        type={"My project"}
+                        github={"https://github.com/Chinweike99/taSty"}
+                        img={assets.tasty}
+                        
+                        />
+                    </div>
+                    <div className='col-span-6'>
+                    <Project 
+                        title={"Food delivery website"}
+                        summary={"Site for buying and selling"}
+                        link={'https://github.com/Chinweike99/taSty'}
+                        type={"My project"}
+                        github={"https://github.com/Chinweike99/taSty"}
+                        img={assets.tasty}
+                        
+                        />
+                    </div>
+
+                    <div className='col-span-12'>
+                    <MyProjects 
                         title={"Food delivery website"}
                         summary={"Site for buying and selling"}
                         link={'https://github.com/Chinweike99/taSty'}
@@ -81,22 +160,27 @@ const projects = () => {
                         />
                     </div>
                     {/* Main Projects */}
-                    <div className='col-span6-'>
-                        Project 1
+                    <div className='col-span-6'>
+                    <Project 
+                        title={"Food delivery website"}
+                        summary={"Site for buying and selling"}
+                        link={'https://github.com/Chinweike99/taSty'}
+                        type={"My project"}
+                        github={"https://github.com/Chinweike99/taSty"}
+                        img={assets.tasty}
+                        
+                        />
                     </div>
-                    <div className='col-span6-'>
-                        Project 2
-                    </div>
-
-                    <div className='col-span-12'>
-                        Featured Projects
-                    </div>
-                    {/* Main Projects */}
-                    <div className='col-span6-'>
-                        Project 3
-                    </div>
-                    <div className='col-span6-'>
-                        Project 4
+                    <div className='col-span-6'>
+                    <Project 
+                        title={"Food delivery website"}
+                        summary={"Site for buying and selling"}
+                        link={'https://github.com/Chinweike99/taSty'}
+                        type={"My project"}
+                        github={"https://github.com/Chinweike99/taSty"}
+                        img={assets.tasty}
+                        
+                        />
                     </div>
 
                 </div>
