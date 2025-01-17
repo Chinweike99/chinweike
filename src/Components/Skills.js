b254472a48ecfe2b265f9dae173139b8608930b9
Chinweike99 <innocentakwolu1@gmail.com>
2025-01-17 16:17:34 +0100

    responsiveness

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const MySkills = ({ name, x, y }) => {
   return (
     <motion.div
-      className="flex items-center justify-center rounded-full bg-dark text-light py-3 px-6 font-semibold shadow-gray-500 hover:bg-light hover:text-dark border-2 hover:border-dark cursor-pointer absolute "
+      className="flex items-center justify-center rounded-full bg-dark text-light py-3 px-6 font-semibold shadow-gray-500 hover:bg-light hover:text-dark border-2 hover:border-dark cursor-pointer absolute lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:border-none xs:text-dark xs:bg-transparent xs:dark:bg-transparent xs:dark:text-light xs:font-bold xs:text-[1.1rem]"
       whileHover={{ scale: 1.05 }}
       initial={{x:0, y:0}}
       whileInView={{x:x, y:y, transition: {duration: 1.5}}}
@@ -17,25 +17,25 @@ const MySkills = ({ name, x, y }) => {
 const Skills = () => {
   return (
     <>
-      <h2 className="font-bold text-5xl mt-[8rem] mb-5 w-full text-center">
+      <h2 className="font-bold text-5xl mt-[8rem] mb-5 w-full text-center md:text-6xl md:mt-32">
         Skills
       </h2>
       <div className="w-full h-screen relative flex items-center justify-center  bg-circularLight dark:bg-circularDark">
         <motion.div
-          className="flex items-center justify-center rounded-full bg-dark text-light p-3 font-semibold shadow-gray-500 hover:bg-light hover:text-dark border-2 hover:border-dark cursor-pointer"
+          className="flex items-center justify-center rounded-full bg-dark text-light p-3 font-semibold shadow-gray-500 hover:bg-light hover:text-dark border-2 hover:border-dark cursor-pointer xs:p-1"
           whileHover={{ scale: 1.05 }}
         >
           JavaScript
         </motion.div>
 
         <MySkills name={"HTML5/CSS"} x="-23vw" y="-13vw"/>
-        <MySkills name={"TailwindCSS"} x="1vw" y="-18vw"/>
+        <MySkills name={"TailwindCSS"} x="1vw" y="-19vw"/>
         <MySkills name={"ReactJs"} x="25vw" y="-10vw"/>
-        <MySkills name={"NextJs"} x="18vw" y="1vw"/>
-        <MySkills name={"ExpressJs"} x="-18vw" y="1vw"/>
+        <MySkills name={"NextJs"} x="22vw" y="1vw"/>
+        <MySkills name={"ExpressJs"} x="-22vw" y="1vw"/>
         <MySkills name={"NodeJs"} x="-22vw" y="14vw"/>
         <MySkills name={"MongoDB"} x="-1vw" y="18vw"/>
-        <MySkills name={"PostgreSQL"} x="20vw" y="14vw"/>
+        <MySkills name={"PostgreSQL"} x="20vw" y="13vw"/>
       </div>
     </>
   );
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -41,11 +41,11 @@ const profile = () => {
         <title>Chinweikwe | About page</title>
         <meta name="Chinweike" content="about me" />
       </Head>
-      <main className="flex w-full flex-col items-center justify-center dark:text-light">
+      <main className="flex w-full flex-col items-center dark:text-light">
         <Layout className="pt-10">
-          <AnimatedText text={"Passion fuels purpose"} className="mb-10" />
-          <div className="grid w-full grid-cols-8 gap-16">
-            <div className="col-span-3 flex flex-col items-start justify-start text-justify">
+          <AnimatedText text={"Passion fuels purpose"} className="mb-10 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8" />
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8 ">
+            <div className="col-span-3 flex flex-col items-start justify-start text-justify xl:col-span-4 md:order-1 md:col-span-8">
               <h2 className="mb-4 text-lg font-bold  uppercase texxr-dark/75">
                 Biography
               </h2>
@@ -71,32 +71,33 @@ const profile = () => {
                 typesetting, remaining essentially unchanged.
               </p>
             </div>
-            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark dark:border-light bg-light p-8 dark:bg-dark">
-              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark dark:border-light bg-light p-8 dark:bg-dark xl:col-span-4 md:order-1 md:col-span-8">
+
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light " />
               <Image src={assets.must2} alt="profile-image" className="w-full h-auto rounded-2xl" priority sizes="(max-width: 760px) 100vw, (max-width: 1200px) 50vw, 33vw" />
             </div>
 
-            <div className="col-span-2 flex flex-col items-end justify-between ">
+            <div className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3">
 
-                <div className="flex flex-col items-center justify-center">
-                    <span className="inline-block text-5xl font-bold">
+                <div className="flex flex-col items-center justify-center xl:items-center md:items-center">
+                    <span className="inline-block text-5xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
                         <AnimatedNumbers value={20}/>+
                     </span>
-                    <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">Satisfied clients</h2>
+                    <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">Satisfied clients</h2>
                 </div>
 
-                <div className="flex flex-col items-center justify-center">
-                    <span className="inline-block text-5xl font-bold">
+                <div className="flex flex-col items-center justify-center xl:items-center">
+                    <span className="inline-block text-5xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
                         <AnimatedNumbers value={30}/>+
                     </span>
-                    <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">Projects completed</h2>
+                    <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">Projects completed</h2>
                 </div>
 
-                <div className="flex flex-col items-center justify-center">
-                    <span className="inline-block text-5xl font-bold">
+                <div className="flex flex-col items-center justify-center xl:items-center">
+                    <span className="inline-block text-5xl font-bold xl:items-center md:text-6xl sm:text-5xl xs:text-4xl">
                         <AnimatedNumbers value={2}/>+
                     </span>
-                    <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">Years of experience</h2>
+                    <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">Years of experience</h2>
                 </div>
 
             </div>
